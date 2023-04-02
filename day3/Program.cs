1b2ff9a1771cb4d0131c2b7583ee302bd8ff089c
kiran <example.1>
2023-04-02 09:11:41 +0545

    Add project files.

---
+++ day3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //try//exception handling
+          //  {
+          //      Console.WriteLine("enter a number");
+          //      string givenString=Console.ReadLine();
+          //      int myNmuber=Convert.ToInt32(givenString);
+          //      Console.WriteLine(myNmuber);
+          //      string[] names = new string[3];
+          //      names[0] = "Bilapa";
+          //      names[1] = "Anju";
+          //      names[2] = "Astik";
+          //      //names[3] = "Ranju";
+          //      foreach (string item in names)
+          //      {
+          //          Console.WriteLine(item);
+          //      }
+          //   }
+          //   catch(InsufficientMemoryException ex)
+          //  {
+          //      Console.WriteLine(ex.Message);
+          //  }
+          //  catch(Exception ex)
+          //  {
+          //      Console.WriteLine(ex.Message);
+          //  }
+          Cow cow=new Cow();
+            cow.WhatIeat();
+            cow.WhatIeat("straw");
+            cow.WhatIeat("straw", "grass");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ day3/animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+ public abstract class animal 
+    {
+        public void WhatIeat()
+        {
+            Console.WriteLine("I eat grass");
+
+        }
+        public abstract void WhatIeat(string food);
+        //{
+        //    Console.WriteLine("I eat " + food);
+        //}
+        public void WhatIeat(string food1, string food2)
+        {
+            Console.WriteLine("I eat " + food1 +" and " + food2);
+        }
+    }
+        public class Cow : animal
+        {
+            public override void WhatIeat(string food)
+            {
+                Console.WriteLine("I eat "+ food);
+            }
+        }
+}
